fd897fbbe401d6c29aae694478c0842ebed1e9c4
Alisson <filhoalisso@gmail.com>
2026-03-14 12:01:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+const habilidades = document.querySelectorAll(".habilidade");
+
+habilidades.forEach(card => {
+
+card.addEventListener("click", () => {
+
+card.classList.toggle("ativo");
+
+});
+
+});
+
+particlesJS("particles-js", {
+
+particles: {
+
+number: {
+value: 80
+},
+
+color: {
+value: "#38bdf8"
+},
+
+shape: {
+type: "circle"
+},
+
+opacity: {
+value: 0.5
+},
+
+size: {
+value: 3
+},
+
+line_linked: {
+enable: true,
+distance: 150,
+color: "#38bdf8",
+opacity: 0.4
+},
+
+move: {
+enable: true,
+speed: 2
+}
+
+}
+
+});
+
+const texto = "Alisson Sousa";
+let index = 0;
+
+function escrever(){
+
+if(index < texto.length){
+
+document.getElementById("typing").innerHTML += texto.charAt(index);
+
+index++;
+
+setTimeout(escrever,150);
+
+}
+
+}
+
+escrever();
